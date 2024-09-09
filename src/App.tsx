1bea58f5917d09acfb7c19687634c1acb26b2493
yae <sky.bgm.tdrksht.132133@gmail.com>
2024-09-10 03:25:21 +0900

    アプリのひな型作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultValue = { content: "", hour: 0 };
+  const [formValue, setFormValue] = useState(defaultValue);
+  const [sum, setSum] = useState(0);
+  const [isError, setIsError] = useState(false);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label>
+          学習内容
+          <input
+            name="study-content"
+            value={formValue.content}
+            onChange={(e) =>
+              setFormValue((prev) => ({ ...prev, content: e.target.value }))
+            }
+          />
+        </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <label>
+          学習時間
+          <input
+            type="number"
+            name="study-hour"
+            value={formValue.hour}
+            min={0}
+            onChange={(e) =>
+              setFormValue((prev) => ({
+                ...prev,
+                hour: Number(e.target.value),
+              }))
+            }
+          />
+          時間
+        </label>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>入力されている学習内容：{formValue.content}</div>
+      <div>入力されている学習時間：{formValue.hour}</div>
+      <input
+        type="submit"
+        value="登録"
+        onClick={() => {
+          if (formValue.content === "" || formValue.hour === 0)
+            setIsError(true);
+          else {
+            setIsError(false);
+            setSum((sum) => sum + formValue.hour);
+            setFormValue(defaultValue);
+          }
+        }}
+      />
+      {isError && <div>入力されていない項目があります</div>}
+      <div>合計時間：{sum}/1000(h)</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
